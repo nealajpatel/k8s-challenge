45ebbb5ce3cc883985e2a05056133fc7a57b3dd5
Neal Patel <neal@siimpl.io>
2024-05-08 18:17:38 -0400

    testing ci validation

--- providers.tf
+++ providers.tf
@@ -1,36 +1,36 @@
 terraform {
   required_providers {
     kind = {
-      source = "tehcyx/kind"
+      source  = "tehcyx/kind"
       version = "0.4.0"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
       version = "1.14.0"
     }
   }
 }
 
 provider "kubernetes" {
-  host = "${kind_cluster.default.endpoint}"
-  cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
-  client_certificate = "${kind_cluster.default.client_certificate}"
-  client_key = "${kind_cluster.default.client_key}"
+  host                   = kind_cluster.default.endpoint
+  cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
+  client_certificate     = kind_cluster.default.client_certificate
+  client_key             = kind_cluster.default.client_key
 }
 
 provider "kubectl" {
-  host = "${kind_cluster.default.endpoint}"
-  cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
-  client_certificate = "${kind_cluster.default.client_certificate}"
-  client_key = "${kind_cluster.default.client_key}"
+  host                   = kind_cluster.default.endpoint
+  cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
+  client_certificate     = kind_cluster.default.client_certificate
+  client_key             = kind_cluster.default.client_key
 }
 
 provider "helm" {
   kubernetes {
-    host = "${kind_cluster.default.endpoint}"
-    cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
-    client_certificate = "${kind_cluster.default.client_certificate}"
-    client_key = "${kind_cluster.default.client_key}"
+    host                   = kind_cluster.default.endpoint
+    cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
+    client_certificate     = kind_cluster.default.client_certificate
+    client_key             = kind_cluster.default.client_key
   }
 }
 
